a71a6d6fed2ecfc52326a448090634927f462adb
melihemre2001 <melihemre2001@hotmail.com>
2023-05-13 23:52:22 +0300

    responsive sandwich navbar

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+const navbar = document.getElementsByClassName("navbar")[0];
 
 toggleButton.addEventListener("click", function () {
-  navbarLinks.classList.toggle("active");
+  navbar.classList.toggle("active");
 });
